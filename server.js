d90cd812cfc8614f36b8ee1baba124449449938f
naai-repo <122557599+naai-repo@users.noreply.github.com>
2023-07-10 15:28:50 +0530

    db connect

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
-const express = require('express');
+const express = require('express')
+const {MongoClient} = require('mongodb');
+// const url = 'mongodb://localhost:27017?directConnection=true';
+const url = 'mongodb://0.0.0.0:27017';
+const dbName = 'Naai'
+const client = new MongoClient(url);
+const app = express()
+const port = 3000
 
-const app = express();
-const PORT = 3000;
 
-app.get('/hello', (req, res)=>{
-	res.set('Content-Type', 'text/html');
-	res.status(200).send("<h1>Hello!</h1>");
-});
 
-app.listen(PORT, (error) =>{
-	if(!error)
-		console.log("Server is Successfully Running, and App is
-					listening on port "+ PORT)
-	else
-		console.log("Error occurred, server can't start", error);
-	}
-);
+async function getData(){
+
+    let res = await client.connect();
+    db=res.db(dbName)
+    collection = db.collection('users')
+    let data  = await collection.find({}).toArray();
+    console.log('ew')
+    console.log(data)
+    return data;
+
+}
+
+getData()
+
+app.get('/', async (req, res) => {
+  let data = await getData();
+  res.json(data)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
