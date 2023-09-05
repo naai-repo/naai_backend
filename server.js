eebe9ecb406031e1134ef141416621ddc919f595
naai-repo <122557599+naai-repo@users.noreply.github.com>
2023-09-05 17:13:02 +0530

    Update server.js getArtists

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const {MongoClient} = require('mongodb');
+ const Review = require('./model/Review')
 // const url = 'mongodb://localhost:27017?directConnection=true';
-const url = 'mongodb://0.0.0.0:27017';
+const url = 'mongodb://0.0.0.0:27017/?directConnection=true';
 const dbName = 'Naai'
 const client = new MongoClient(url);
 const app = express()
 const port = 3000
+
+const newExample = new Review({
+  title: 'John Doe', 
+  userId: new mongoose.Types.ObjectId(),
+   saloonId: new mongoose.Types.ObjectId(),
+   artistId :new mongoose.Types.ObjectId(),
+});
+
+
+newExample.save()
+  .then(result => {
+    console.log('Document saved:', result);
+  })
+  .catch(err => {
+    console.error(err);
+  });
 
 
 
@@ -20,6 +38,16 @@ async function getData(){
     return data;
 
 }
+async function getArtists(){
+
+    let res = await client.connect();
+    db=res.db(dbName)
+    collection = db.collection('artist')
+    let data  = await collection.find({}).toArray();
+    console.log(data)
+    return data;
+
+}
 
 getData()
 
@@ -29,6 +57,11 @@ app.get('/', async (req, res) => {
 })
 app.get('/createUser', async (req, res) => {
   let data = await getData();
+  res.json(data)
+})
+
+app.get('/v1/getArtists', async (req, res) => {
+  let data = await getArtists();
   res.json(data)
 })
 
