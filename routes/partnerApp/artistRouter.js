39d9082b776dd5e8861633ea6d8a8fb6537f5f72
Deepanshu Tamta <76214277+Dtamta007@users.noreply.github.com>
2024-01-16 03:59:02 +0530

    Dev (#36)
    * Searching Functionality added + Stable + 19Nov
    
    * Booking Appointments Made + Salon industry revolutionized (#21)
    
    * Booking upd (#23)
    
    * Booking Appointments Made + Salon industry revolutionized
    
    * Booking update
    
    * Delete routes/bookingRoutes/booking.js
    
    * Single artist booking (#25)
    
    * Searching Functionality added + Stable + 19Nov (#20)
    
    * Dev (#22)
    
    * Searching Functionality added + Stable + 19Nov
    
    * Booking Appointments Made + Salon industry revolutionized (#21)
    
    * Dev (#24)
    
    * Searching Functionality added + Stable + 19Nov
    
    * Booking Appointments Made + Salon industry revolutionized (#21)
    
    * Booking upd (#23)
    
    * Booking Appointments Made + Salon industry revolutionized
    
    * Booking update
    
    * Delete routes/bookingRoutes/booking.js
    
    * Delete routes/bookingRoutes/booking.js
    
    * Single Artist Booking Sorted
    
    * Single booking upd (#27)
    
    * Dev (#26)
    
    * Searching Functionality added + Stable + 19Nov
    
    * Booking Appointments Made + Salon industry revolutionized (#21)
    
    * Booking upd (#23)
    
    * Booking Appointments Made + Salon industry revolutionized
    
    * Booking update
    
    * Delete routes/bookingRoutes/booking.js
    
    * Single artist booking (#25)
    
    * Searching Functionality added + Stable + 19Nov (#20)
    
    * Dev (#22)
    
    * Searching Functionality added + Stable + 19Nov
    
    * Booking Appointments Made + Salon industry revolutionized (#21)
    
    * Dev (#24)
    
    * Searching Functionality added + Stable + 19Nov
    
    * Booking Appointments Made + Salon industry revolutionized (#21)
    
    * Booking upd (#23)
    
    * Booking Appointments Made + Salon industry revolutionized
    
    * Booking update
    
    * Delete routes/bookingRoutes/booking.js
    
    * Delete routes/bookingRoutes/booking.js
    
    * Single Artist Booking Sorted
    
    * Single Booking Upd
    
    * Login upd (#29)
    
    * Dev (#26)
    
    * Searching Functionality added + Stable + 19Nov
    
    * Booking Appointments Made + Salon industry revolutionized (#21)
    
    * Booking upd (#23)
    
    * Booking Appointments Made + Salon industry revolutionized
    
    * Booking update
    
    * Delete routes/bookingRoutes/booking.js
    
    * Single artist booking (#25)
    
    * Searching Functionality added + Stable + 19Nov (#20)
    
    * Dev (#22)
    
    * Searching Functionality added + Stable + 19Nov
    
    * Booking Appointments Made + Salon industry revolutionized (#21)
    
    * Dev (#24)
    
    * Searching Functionality added + Stable + 19Nov
    
    * Booking Appointments Made + Salon industry revolutionized (#21)
    
    * Booking upd (#23)
    
    * Booking Appointments Made + Salon industry revolutionized
    
    * Booking update
    
    * Delete routes/bookingRoutes/booking.js
    
    * Delete routes/bookingRoutes/booking.js
    
    * Single Artist Booking Sorted
    
    * Single Booking Upd
    
    * Login routes updated
    
    * Login upd12jan (#31)
    
    * Single Booking Upd
    
    * Login routes updated
    
    * Login Routes UPD
    
    * Top salons (#33)
    
    * Single Booking Upd
    
    * Login routes updated
    
    * Login Routes UPD
    
    * Top salons and artists logic
    
    * Filter routes (#35)
    
    * Single Booking Upd
    
    * Login routes updated
    
    * Login Routes UPD
    
    * Top salons and artists logic
    
    * Filter routes added for Artists and salons

--- routes/partnerApp/artistRouter.js
+++ routes/partnerApp/artistRouter.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const mongoose = require("mongoose");
 const Artist = require("../../model/partnerApp/Artist");
 const Salon = require("../../model/partnerApp/Salon");
+const Booking = require("../../model/partnerApp/Booking");
 const wrapperMessage = require("../../helper/wrapperMessage");
 const jwtVerify = require("../../middleware/jwtVerification");
 
@@ -73,13 +74,88 @@ router.post("/:id/update", async (req, res) => {
   }
 });
 
-router.get('/topArtists', jwtVerify, async (req,res) => {
+router.post('/topArtists', async (req,res) => {
   try{
-      let location = req.user.location;
+      let location = req.body.location; 
       let page = Number(req.query.page) || 1;
       let limit = Number(req.query.limit) || 15;
       let skip = (page-1)*limit;
-      let data = await Artist.aggregate([
+      let artists = await Artist.aggregate([
+          {
+              "$geoNear": {
+                  "near": location,
+                  "distanceField": "distance",
+                  "distanceMultiplier": 0.001
+              }
+          }
+      ]);
+      let totalBookings = await Booking.find().count("val");
+      let totalArtists = await Artist.find().count("artist");
+      let maxDistance = 0;
+      let maxRating = 5;
+      let avgBookings = totalBookings / totalArtists;
+      let end = 0;
+      if(artists.length < 1000){
+        maxDistance = artists[artists.length - 1].distance
+        end = artists.length-1;
+      }else{
+        maxDistance = artists[1000].distance
+        end = 1000;
+      }
+
+      for(let itr = 0; itr <= end; itr++ ){
+        let artist = artists[itr];
+        let bookings = artist.bookings >= avgBookings ? avgBookings : artist.bookings;
+        let score = 0;
+        score = (((maxDistance - artist.distance) / maxDistance)*0.5)
+                + ((artist.rating / maxRating)*0.3)
+                + ((bookings / avgBookings)*0.2);
+        
+        if(artist.paid){
+          score += score*0.4
+        }
+
+        artists[itr]["score"] = score;
+      }
+      artists.sort((a,b) => {
+        if(a.score < b.score)
+          return 1
+        else if (a.score > b.score)
+          return -1
+        else return 0
+      })
+      let data = [];
+      for(let itr = skip; itr<skip+limit; itr++){
+        if(!artists[itr]){
+          break;
+        }
+        data.push(artists[itr]);
+      }
+      res.status(200).json(wrapperMessage("success", "", data));
+  }catch(err){
+      console.log(err);
+      res.status(500).json(wrapperMessage("failed", err.message));
+  }
+})
+
+router.post("/filter", async (req,res) => {
+  try{
+      let filter = req.query.filter.toLowerCase();
+      if(!filter){
+          let error = new Error("Invalid filter selected!");
+          error.code = 400;
+          throw error;
+      }
+      let location = req.body.location;
+      if(!location){
+          let error = new Error("Invalid location selected!");
+          error.code = 400;
+          throw error;
+      }
+      let page = Number(req.query.page) || 1;
+      let limit = Number(req.query.limit) || 3;
+      let skip = (page-1)*limit;
+      let artists = await Artist.aggregate([
           {
               "$geoNear": {
                   "near": location,
@@ -87,31 +163,51 @@ router.get('/topArtists', jwtVerify, async (req,res) => {
                   "distanceMultiplier": 0.001
               }
           },
-          {
-              $sort: {
-                  bookings: -1
+      ]);
+      let maxDistance = 0;
+      let end = 0;
+      if(artists.length < 1000){
+          maxDistance = artists[artists.length - 1].distance
+          end = artists.length-1;
+      }else{
+          maxDistance = artists[1000].distance
+          end = 1000;
+      }
+
+      if(filter === "rating"){
+          let maxRating = 5;
+          for(let itr = 0; itr <= end; itr++ ){
+              let artist = artists[itr];
+              let score = 0;
+              score = (((maxDistance - artist.distance) / maxDistance)*0.4)
+                      + ((artist.rating / maxRating)*0.4);
+              
+              if(artist.paid){
+                score += 0.2
               }
-          },
-          {
-              $sort: {
-                  rating: -1
-              }
-          },
-          {
-              $sort: {
-                  paid: -1
-              }
-          },
-          // {
-          //     $sort: {
-          //         distance: 1
-          //     }
-          // },
-      ]).skip(skip).limit(limit);
-      res.status(200).json(data);
+  
+              artists[itr]["score"] = score;
+          }
+      }
+
+      artists.sort((a,b) => {
+          if(a.score < b.score)
+          return 1
+          else if (a.score > b.score)
+          return -1
+          else return 0
+      })
+      let data = [];
+      for(let itr = skip; itr<skip+limit; itr++){
+          if(!artists[itr]){
+          break;
+          }
+          data.push(artists[itr]);
+      }
+      res.status(200).json(wrapperMessage("success", "", data));
   }catch(err){
       console.log(err);
-      res.status(500).json(wrapperMessage("failed", err.message));
+      res.status(err.code || 500).json(wrapperMessage("failed", err.message));
   }
 })
 
