8046afb84eec860e21905d16f5dc157dffbeff76
Dtamta007 <dtamta007@gmail.com>
2024-03-08 19:36:16 +0530

    Adds files and code for deep linking

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const passport = require('passport');
-const session = require('express-session')
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+require("dotenv").config();
 const app = express();
 const PORT = 8800;
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rg1ncj1.mongodb.net/${process.env.DB_NAME}`;
 
 // Requiring routers
 // Admin Routes
-const AdminRouter = require('./routes/adminRoutes/livePermissionRouter');
+const AdminRouter = require("./routes/adminRoutes/livePermissionRouter");
 // Image Routes
-const ImageRouter = require('./routes/imageRoutes/imageRouter');
+const ImageRouter = require("./routes/imageRoutes/imageRouter");
 // Partner App
-const ReviewRouter = require('./routes/partnerApp/reviewRouter');
-const SalonRouter = require('./routes/partnerApp/salonRouter');
-const ServiceRouter = require('./routes/partnerApp/serviceRouter');
-const ArtistRouter = require('./routes/partnerApp/artistRouter');
-const PartnerRouter = require('./routes/partnerApp/partnerRouter')
-const OtpRouter = require('./routes/partnerApp/otpRouter');
-const InventoryRouter = require('./routes/partnerApp/inventoryRouter');
-const PlanRouter = require('./routes/partnerApp/planRouter');
+const ReviewRouter = require("./routes/partnerApp/reviewRouter");
+const SalonRouter = require("./routes/partnerApp/salonRouter");
+const ServiceRouter = require("./routes/partnerApp/serviceRouter");
+const ArtistRouter = require("./routes/partnerApp/artistRouter");
+const PartnerRouter = require("./routes/partnerApp/partnerRouter");
+const OtpRouter = require("./routes/partnerApp/otpRouter");
+const InventoryRouter = require("./routes/partnerApp/inventoryRouter");
+const PlanRouter = require("./routes/partnerApp/planRouter");
 
 // Customer App
-const UserRouter = require('./routes/customerApp/userRouter');
-const UserOtpRouter = require('./routes/customerApp/otpRouter');
-const LocationRouter = require('./routes/customerApp/locationRouter');
+const UserRouter = require("./routes/customerApp/userRouter");
+const UserOtpRouter = require("./routes/customerApp/otpRouter");
+const LocationRouter = require("./routes/customerApp/locationRouter");
 
 // Booking Appointment
-const SchedulingRouter = require('./routes/bookingRoutes/schedulingRoutes');
+const SchedulingRouter = require("./routes/bookingRoutes/schedulingRoutes");
 
 // MiddleWares
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}))
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -46,7 +48,7 @@ app.use(passport.session());
 app.use("/admin", AdminRouter);
 
 // Image Routes
-app.use("/image", ImageRouter)
+app.use("/image", ImageRouter);
 
 // Partner App Routes
 app.use("/partner/review", ReviewRouter);
@@ -66,21 +68,34 @@ app.use("/customer/plan", PlanRouter);
 // Scheduling Appointments
 app.use("/appointments", SchedulingRouter);
 
-app.get('/', async (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+app.get("/", async (req, res) => {
+  res.sendFile(__dirname + "/index.html");
   // res.send("Welcome to backend");
-})
+});
+
+// Deep linking routes
+app.get("/salon/:id", (req, res) => {
+  res.sendFile(__dirname + "/deep-linking/salon.html");
+});
+
+app.get("/artist/:id", (req, res) => {
+  res.sendFile(__dirname + "/deep-linking/artist.html");
+});
+
+app.get("/.well-known/assetlinks.json", (req, res) => {
+  res.sendFile(__dirname + "/deep-linking/assetlinks.json");
+});
 
 app.listen(PORT, async () => {
-  try{
+  try {
     //connecting to database
     let db = await mongoose.connect(url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-    console.log('Connected to Database!');
-    console.log(`App listening on port ${PORT}`)
-  }catch(err){
+    console.log("Connected to Database!");
+    console.log(`App listening on port ${PORT}`);
+  } catch (err) {
     console.log(err);
   }
-})
+});
