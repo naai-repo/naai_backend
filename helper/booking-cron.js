7a72f86d69e0625a456cbaf89e2cd6b4d303100e
Dtamta007 <dtamta007@gmail.com>
2024-03-13 00:24:11 +0530

    Adds cron for discounts

--- helper/booking-cron.js
+++ helper/booking-cron.js
@@ -1,6 +1,8 @@
 const cron = require('node-cron');
 const mongoose = require('mongoose');
 const Booking = require('../model/partnerApp/Booking'); // Assuming your Booking model is defined in a separate file
+const Salon = require('../model/partnerApp/Salon');
+const CommonUtils = require('./commonUtils');
 
 
 console.log('hi from cron')
@@ -40,4 +42,31 @@ cron.schedule('*/10 * * * *', async () => {
     } catch (error) {
         console.error(error);
     }
+});
+
+cron.schedule('0 0 * * *', async () => {
+    try {
+        const currentTime = new Date().toISOString();
+        console.log('Current Time', currentTime);
+
+        // Find discounts that have endDate of currentTime or less than currentTime
+        const discountsToUpdate = await Salon.find({
+            $and: [
+                { "discountTime.end": { $lte: currentTime } },
+                { "discountTime.end": { $ne: "" } }
+            ]
+        });
+        let salonIdArr = discountsToUpdate.map(salon => salon._id);
+        await Promise.all(discountsToUpdate.map(salon => {
+            salon.discount = 0;
+            salon.discountTime = { start: "", end: "" };
+            return salon.save();
+        }));
+        await Promise.all(salonIdArr.map(salonId => {
+            return CommonUtils.updateDiscountedServicePrice(salonId, 0);
+        }));
+        console.log(`${discountsToUpdate.length} Salon Discounts ended!`);
+    } catch (error) {
+        console.error(error);
+    }   
 });
