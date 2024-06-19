f41febc0ed0145894a5ab8912d3b1eb396a19c1a
Deepanshu Tamta <76214277+Dtamta007@users.noreply.github.com>
2024-06-19 21:54:24 +0530

    Upd19jun (#219)
    * Adds booking info route for single bookings
    
    * Dev (#215)
    
    * Upd10jun (#209)
    
    * Adds walkin Routes to Backend
    
    * Changes the dev-app.js
    
    * Updates models and routes for POS
    
    * Changes model for salons
    
    * Dashtemp (#212)
    
    * Upd10jun (#209) (#210)
    
    * Adds walkin Routes to Backend
    
    * Changes the dev-app.js
    
    * Updates models and routes for POS
    
    * Changes model for salons
    
    * booking id and userId added in dashboard data
    
    ---------
    
    Co-authored-by: naai-repo <122557599+naai-repo@users.noreply.github.com>
    Co-authored-by: shubh <ssagar8186@gmail.com>
    
    * Upd13jun (#214)
    
    * Upd10jun (#209) (#210)
    
    * Adds walkin Routes to Backend
    
    * Changes the dev-app.js
    
    * Updates models and routes for POS
    
    * Changes model for salons
    
    * Adds booking info route for single bookings
    
    ---------
    
    Co-authored-by: naai-repo <122557599+naai-repo@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: naai-repo <122557599+naai-repo@users.noreply.github.com>
    Co-authored-by: shubh <ssagar8186@gmail.com>
    
    * Dev (#217)
    
    * Upd10jun (#209)
    
    * Adds walkin Routes to Backend
    
    * Changes the dev-app.js
    
    * Updates models and routes for POS
    
    * Changes model for salons
    
    * Dashtemp (#212)
    
    * Upd10jun (#209) (#210)
    
    * Adds walkin Routes to Backend
    
    * Changes the dev-app.js
    
    * Updates models and routes for POS
    
    * Changes model for salons
    
    * booking id and userId added in dashboard data
    
    ---------
    
    Co-authored-by: naai-repo <122557599+naai-repo@users.noreply.github.com>
    Co-authored-by: shubh <ssagar8186@gmail.com>
    
    * Upd13jun (#214)
    
    * Upd10jun (#209) (#210)
    
    * Adds walkin Routes to Backend
    
    * Changes the dev-app.js
    
    * Updates models and routes for POS
    
    * Changes model for salons
    
    * Adds booking info route for single bookings
    
    ---------
    
    Co-authored-by: naai-repo <122557599+naai-repo@users.noreply.github.com>
    
    * Upd16jun (#216)
    
    * Adds booking info route for single bookings
    
    * Dev (#215)
    
    * Upd10jun (#209)
    
    * Adds walkin Routes to Backend
    
    * Changes the dev-app.js
    
    * Updates models and routes for POS
    
    * Changes model for salons
    
    * Dashtemp (#212)
    
    * Upd10jun (#209) (#210)
    
    * Adds walkin Routes to Backend
    
    * Changes the dev-app.js
    
    * Updates models and routes for POS
    
    * Changes model for salons
    
    * booking id and userId added in dashboard data
    
    ---------
    
    Co-authored-by: naai-repo <122557599+naai-repo@users.noreply.github.com>
    Co-authored-by: shubh <ssagar8186@gmail.com>
    
    * Upd13jun (#214)
    
    * Upd10jun (#209) (#210)
    
    * Adds walkin Routes to Backend
    
    * Changes the dev-app.js
    
    * Updates models and routes for POS
    
    * Changes model for salons
    
    * Adds booking info route for single bookings
    
    ---------
    
    Co-authored-by: naai-repo <122557599+naai-repo@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: naai-repo <122557599+naai-repo@users.noreply.github.com>
    Co-authored-by: shubh <ssagar8186@gmail.com>
    
    * Adds tax to the booking model
    
    ---------
    
    Co-authored-by: naai-repo <122557599+naai-repo@users.noreply.github.com>
    Co-authored-by: shubh <ssagar8186@gmail.com>
    
    ---------
    
    Co-authored-by: naai-repo <122557599+naai-repo@users.noreply.github.com>
    Co-authored-by: shubh <ssagar8186@gmail.com>
    
    * Adds searching service from single salon route
    
    ---------
    
    Co-authored-by: naai-repo <122557599+naai-repo@users.noreply.github.com>
    Co-authored-by: shubh <ssagar8186@gmail.com>

--- controllers/posController/service.controller.js
+++ controllers/posController/service.controller.js
@@ -1,34 +1,79 @@
 const wrapperMessage = require("../../helper/wrapperMessage");
 const Service = require("../../model/partnerApp/Service");
 
-exports.getServicesFromCategories = async(req, res, next) => {
-    try{
-        let salonId = req.query.salonId;
-        const sex = req.query.sex;
-        let category = req.params.category;
-        let queryObject = {
-            salonId
-        };
-        if(!salonId){
-            let err = new Error("Salon ID is required!");
-            err.code = 400;
-            throw err;
-        }
-
-        if(category){
-            queryObject.category = {$regex: category, $options: "i"};
-            queryObject.targetGender = sex;
-        }
-
-        let services = await Service.find(queryObject);
-        if(services.length === 0){
-            let err = new Error("No services found!");
-            err.code = 404;
-            throw err;
-        }
-        res.status(200).json(wrapperMessage("success", "Services fetched successfully!", {services, count: services.length}));
-    }catch(err){
-        console.log(err);
-        return res.status(err.code || 500).json(wrapperMessage("failed", err.message));
+exports.getServicesFromCategories = async (req, res, next) => {
+  try {
+    let salonId = req.query.salonId;
+    const sex = req.query.sex;
+    let category = req.params.category;
+    let queryObject = {
+      salonId,
+    };
+    if (!salonId) {
+      let err = new Error("Salon ID is required!");
+      err.code = 400;
+      throw err;
     }
-}
+
+    if (category) {
+      queryObject.category = { $regex: category, $options: "i" };
+      queryObject.targetGender = sex;
+    }
+
+    let services = await Service.find(queryObject);
+    if (services.length === 0) {
+      let err = new Error("No services found!");
+      err.code = 404;
+      throw err;
+    }
+    res
+      .status(200)
+      .json(
+        wrapperMessage("success", "Services fetched successfully!", {
+          services,
+          count: services.length,
+        })
+      );
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(err.code || 500)
+      .json(wrapperMessage("failed", err.message));
+  }
+};
+
+exports.searchSingleSalonServices = async (req, res, next) => {
+  try {
+    let salonId = req.query.salonId;
+    let name = req.query.name;
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 3;
+    let skip = (page - 1) * limit;
+    let queryObject = [];
+    if (name) {
+      queryObject.push({ serviceTitle: { $regex: name, $options: "i" } });
+      queryObject.push({ category: { $regex: name, $options: "i" } });
+    }
+    let services = await Service.find({
+      $and: [{ salonId: salonId }, { $or: queryObject }],
+    }).skip(skip).limit(limit);
+    if (services.length === 0) {
+      let err = new Error("No services found!");
+      err.code = 404;
+      throw err;
+    }
+    res
+      .status(200)
+      .json(
+        wrapperMessage("success", "Services fetched successfully!", {
+          services,
+          count: services.length,
+        })
+      );
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(err.code || 500)
+      .json(wrapperMessage("failed", err.message));
+  }
+};
