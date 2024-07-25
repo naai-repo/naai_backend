d07a30d8dd73e99627f3dea2919866fe36c6219b
Dtamta007 <dtamta007@gmail.com>
2024-07-25 21:52:40 +0530

    Adds routes for membership

---
+++ model/membership/Membership.model.js
@@ -0,0 +1,155 @@
+const mongoose = require('mongoose');
+
+const MembershipSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    salonId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Salon',
+        default: process.env.NULL_OBJECT_ID,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    validity_in_days: {
+        type: Number,
+        required: true,
+    },
+    validity_unit: {
+        type: String,
+        required: true,
+        enum: ['DAY', 'MONTH', 'YEAR'],
+    },
+    cost: {
+        type: Number,
+        required: true,
+    },
+    wallet_amount_credit: {
+        type: Number,
+        default: 0,
+    },
+    min_bill_amount: {
+        type: Number,
+        default: 0,
+    },
+    discount_type: {
+        type: Number,
+        default: null,
+    },
+    discount_type_value: {
+        type: Number,
+        default: null,
+    },
+    max_discount_amount: {
+        type: Number,
+        default: null,
+    },
+    all_services_discount_type: {
+        type: Number,
+        default: null,
+    },
+    all_services_discount_type_value: {
+        type: Number,
+        default: null,
+    },
+    all_services_include: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Service',
+    }],
+    all_services_except: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Service',
+    }],
+    all_products_discount_type: {
+        type: Number,
+        default: null,
+    },
+    all_products_discount_type_value: {
+        type: Number,
+        default: null,
+    },
+    all_products_include: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+    }],
+    all_products_except: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+    }],
+    minimum_service_cost: {
+        type: Number,
+        default: null,
+    },
+    minimum_product_cost: {
+        type: Number,
+        default: null,
+    },
+    services: [
+        {
+            id: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Service',
+            },
+            allotted_count: {
+                type: Number,
+                required: true,
+            },
+            discount_type: {
+                type: Number,
+                required: true,
+            },
+            discount_type_value: {
+                type: Number,
+                required: true,
+            },
+            max_discount_amount: {
+                type: Number,
+                default: null,
+            },
+        },
+    ],
+    products: [
+        {
+            id: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product',
+            },
+            allotted_count: {
+                type: Number,
+                required: true,
+            },
+            discount_type: {
+                type: Number,
+                required: true,
+            },
+            discount_type_value: {
+                type: Number,
+                required: true,
+            },
+            max_discount_amount: {
+                type: Number,
+                default: null,
+            },
+        }
+    ],
+    status: {
+        type: Boolean,
+        required: true,
+    },
+    all_services_discount_max_count: {
+        type: Number,
+        default: null,
+    },
+    all_products_discount_max_count: {
+        type: Number,
+        default: null,
+    },
+}, {
+    timestamps: true,
+});
+
+const Memberships = new mongoose.model('Membership', MembershipSchema);
+module.exports = Memberships;
